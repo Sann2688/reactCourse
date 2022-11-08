afba02b4fe3f5595f17a9745a053e9db751e9cbf
olkos <olkos@smile.fr>
2022-11-08 10:29:36 +0200

    practice 5

--- src/App.js
+++ src/App.js
@@ -1,48 +1,10 @@
-import React, {useState} from 'react';
-
-import MoviesList from './components/MoviesList';
-import './App.css';
+import SimpleInput from './components/SimpleInput';
 
 function App() {
-  const [movies, setMovies] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
-  async function fetchMoviesHandler() {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch('https://swapi.dev/api/films/');
-      if (!response.ok) {
-        throw new Error('Something went wrong!')
-      }
-      const data = await response.json();
-      const preparedMovies = data.results.map(movieData => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_data
-        }
-      });
-      setMovies(preparedMovies);
-      setIsLoading(false);
-    } catch (error) {
-      setError(error.message);
-      setIsLoading(false);
-    }
-  }
-
   return (
-    <React.Fragment>
-      <section>
-        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
-      </section>
-      <section>
-        {(!isLoading && error) ? <p>{error}</p> : ''}
-        {isLoading ? <p>LOADING...</p>: <MoviesList movies={movies} />}
-        {(!isLoading && !movies.length && !error) ? <p>No Movies.</p> : ''}
-      </section>
-    </React.Fragment>
+    <div className="app">
+      <SimpleInput />
+    </div>
   );
 }
 
